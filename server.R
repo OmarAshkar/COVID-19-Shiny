12ec122dd5021f8a4290ffa78fc0636521d26403
omarelashkar <o_elashkar@hotmail.com>
2020-04-17 12:31:25 +0200

    remove unnecessary commented code

--- server.R
+++ server.R
@@ -2,7 +2,6 @@ library(shiny)
 library(plotly)
 library(tidyverse)
 
-
 # Download data 
 ## Get zip file
 #download.file(url = "https://github.com/CSSEGISandData/COVID-19/archive/master.zip" 
@@ -11,14 +10,10 @@ library(tidyverse)
 ## Unzip file 
 #unzip("data/covid_repo.zip", exdir = "data")
 
-## Load all csv files in daily_reports
-
 datanames <- list.files("data/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports", 
                         pattern = "\\.csv$",
                         full.names = T)
 
-
-# pat <- "(?<=/)(\\d\\d)(.*)(?<=\\.csv)"
 
 for(i in 1:60){
     # fname <-  str_extract(string = datanames, 
@@ -34,7 +29,6 @@ for(i in 1:60){
     }
 }
 
-
 for(i in 61:83){
     # fnamey <-  str_extract(string = datanames, 
     #                       pattern = pat)[i]
@@ -47,7 +41,6 @@ for(i in 61:83){
     else {
         y <- bind_rows(y, fname)
     }
-    
 }
 y <- y %>% rename(Last.Update = Last_Update, Province.State = Province_State,
                   Latitude = Lat, Longitude = Long_, Country.Region = Country_Region)
@@ -56,41 +49,6 @@ dat <- bind_rows(x, y) %>% select(2:6, 11)
 
 dat$Last.Update <- as.Date(lubridate::parse_date_time(dat$Last.Update, c("%m/%d/%Y %H:%M", "%m/%d/%y %H:%M", "%Y-%m-%d %H:%M", "%Y-%m-%d %H:%M","%Y-%m-%d %H:%M:%S")))
 
-
-
-
-# # Cleaning date. Till 560 --> %d/%m/%Y %h:% // rest --> automatic as_datetime
-# dat1 <- dat[1:560,]
-# dat2 <- dat[561:7617,]
-# dat3 <- dat[7618:11034,]
-# dat4 <- dat[11035:17102,]
-# dat5 <- dat[17103:28136,]
-# dat6 <- dat[28137:38439,]
-# dat7 <- dat[38440:43358,]
-# dat8 <- dat[43359:45927,]
-# dat9 <- dat[45928:77354,]
-# 
-# dat1$Last.Update <- as.Date(dat1$Last.Update, format = "%m/%d/%Y %H:%M")
-# dat2$Last.Update <- as.Date(dat2$Last.Update)
-# dat3$Last.Update <- as.Date(dat3$Last.Update, format = "%m/%d/%y %H:%M")
-# dat4$Last.Update <- as.Date(dat4$Last.Update)
-# dat5$Last.Update <- as.Date(dat5$Last.Update)
-# dat6$Last.Update <- as.Date(dat6$Last.Update, format = "%m/%d/%y %H:%M")
-# dat7$Last.Update <- as.Date(dat7$Last.Update, format = "%Y-%m-%d %H:%M")
-# dat8$Last.Update <- as.Date(dat8$Last.Update, format = "%m/%d/%Y %H:%M")
-# dat9$Last.Update <- as.Date(dat9$Last.Update, format = "%Y-%m-%d %H:%M:%S")
-# 
-# # which.min(is.na(datall$Last.Update))
-# # which.max(is.na(datall$Last.Update))
-# # table(is.na(datall$Last.Update))
-# 
-# datall <- bind_rows(dat1, dat2, dat3, dat4, dat5, dat6, dat7)
-# datall$Last.Update <- as.Date(format(datall$Last.Update, "%y-%m-%d"))
-# 
-# datall$Last.Update <- strptime(as.character(datall$Last.Update), "%y-%m-%d") %>% as.POSIXct()
-
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
   datmain <- reactive({
@@ -116,14 +74,12 @@ shinyServer(function(input, output) {
       ggplot(dat2()) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
   })
 
-
   output$confirmeda <- renderPlotly({
       ggplotly(g() + geom_bar(aes(y = Confirmed, x = Country.Region), stat = 'identity'))
   })
 
-
   output$deathsa <- renderPlotly({
-  ggplotly(g() + geom_bar(aes(y = Deaths, x = Country.Region), stat = 'identity'))
+    ggplotly(g() + geom_bar(aes(y = Deaths, x = Country.Region), stat = 'identity'))
   })
 
   output$recovereda <- renderPlotly({
@@ -138,8 +94,6 @@ shinyServer(function(input, output) {
       DT::datatable(datmain())
   })
 
-
-  
   output$confirmedt <- renderPlotly({
     ggplotly(
       ggplot(data = datmain()) +
@@ -172,10 +126,6 @@ shinyServer(function(input, output) {
   output$tabt <- DT::renderDT({
       datmain()
     })
-# g2() + geom_line(aes(y = Deaths, x = Last.Update))
-# g2() + geom_line(aes(y = Active, x = Last.Update))
-# g2() + geom_line(aes(y = Recovered, x = Last.Update))
-    
-}) # End of shiny function
+  
+})
 
-# https://www.r-graph-gallery.com/279-plotting-time-series-with-ggplot2.html
